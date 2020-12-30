57335cdd3dd93f01dfb71e64c867c7f3e491a46c
Md Shefat Hasnain <67694473+Hshefat@users.noreply.github.com>
2020-12-30 15:14:54 +0600

    Update SecondFragment.java
    Add Back Button in this fragment

--- app/src/main/java/com/example/fragmenttoactivity/SecondFragment.java
+++ app/src/main/java/com/example/fragmenttoactivity/SecondFragment.java
@@ -20,7 +20,15 @@ public class SecondFragment extends Fragment {
         // Inflate the layout for this fragment
         View fv = inflater.inflate(R.layout.fragment_second, null);
 
-
+        Toolbar mActionBar = fv.findViewById(R.id.profileSecond_toolbar);
+        mActionBar.setNavigationIcon(getActivity().getResources().getDrawable(R.drawable.ic_baseline_arrow_back_24));
+        mActionBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //What to do on back clicked
+                getActivity().onBackPressed();
+            }
+        });
 
         return fv;
 
